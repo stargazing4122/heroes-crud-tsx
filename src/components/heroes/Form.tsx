07f6aca4450be4c60f7e88c19512db84a5def0b1
stargazing4122 <jadenyeiden@gmail.com>
2022-01-11 22:59:01 -0500

    🦚form screen of heroes modified

--- src/components/heroes/Form.tsx
+++ src/components/heroes/Form.tsx
@@ -4,11 +4,19 @@ const Form = () => {
   return (
     <div>
       <form>
+        <label className="form-label">Input a superhero</label>
         <input
           type="text"
           className="form-control mb-3"
-          placeholder="Input a hero"
           autoComplete="off"
+          name="superhero  "
+        />
+        <label className="form-label">Input its publisher</label>
+        <input
+          type="text"
+          className="form-control mb-3"
+          autoComplete="off"
+          name=" publisher "
         />
         <button type="submit" className="btn btn-outline-success mb-3">
           Add
